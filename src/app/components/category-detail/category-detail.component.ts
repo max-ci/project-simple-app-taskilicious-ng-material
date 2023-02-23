1df3ec64b5d91d0b81253c211a269e5e601b84ca
Maksymilian Czarnecki <maksymilian@czarnecki.me>
2023-02-23 20:38:20 +0100

    feat: show team member avatars, fix: delete task loader

--- src/app/components/category-detail/category-detail.component.ts
+++ src/app/components/category-detail/category-detail.component.ts
@@ -1,11 +1,24 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { BehaviorSubject, Observable, Subject, combineLatest } from 'rxjs';
-import { catchError, map, switchMap, take, takeUntil, tap } from 'rxjs/operators';
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  combineLatest,
+  map,
+  switchMap,
+  take,
+  takeUntil,
+  tap,
+  catchError,
+} from 'rxjs';
 import { CategoryModel } from '../../models/category.model';
 import { TaskModel } from '../../models/task.model';
 import { CategoryService } from '../../services/category.service';
 import { TaskService } from '../../services/task.service';
+import { TeamMemberService } from '../../services/team-member.service';
+import { TeamMemberModel } from '../../models/team-member.model';
+import { TaskWithTeamMembersModel } from '../../models/task-with-team-members.model';
 
 @Component({
   selector: 'app-category-detail',
@@ -23,8 +36,10 @@ export class CategoryDetailComponent implements OnDestroy {
   private _loadingTasksSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   public loadingTasks$: Observable<boolean> = this._loadingTasksSubject.asObservable();
 
-  private _loadingDeleteTask: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  public loadingDeleteTask$: Observable<boolean> = this._loadingDeleteTask.asObservable();
+  private _loadingDeleteTask: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(
+    null
+  );
+  public loadingDeleteTask$: Observable<string | null> = this._loadingDeleteTask.asObservable();
 
   readonly category$: Observable<CategoryModel> = this._activatedRoute.params.pipe(
     switchMap((params: Params) => this._categoryService.getOne(params['id'])),
@@ -34,13 +49,24 @@ export class CategoryDetailComponent implements OnDestroy {
     })
   );
 
-  private readonly _tasks$: Observable<TaskModel[]> = combineLatest([
+  private readonly _tasks$: Observable<TaskWithTeamMembersModel[]> = combineLatest([
     this._activatedRoute.params,
     this._taskService.getAll(),
+    this._teamMemberService.getAll(),
   ]).pipe(
     takeUntil(this._destroySubject),
-    map(([params, tasks]: [Params, TaskModel[]]) =>
-      tasks.filter((task) => task.categoryId === params['id'])
+    map(([params, tasks, teamMembers]: [Params, TaskModel[], TeamMemberModel[]]) =>
+      tasks
+        .filter((task: TaskModel) => task.categoryId === params['id'])
+        .map((task: TaskModel) => {
+          const teamMembersAssignedToTask: TeamMemberModel[] = Array.isArray(task?.teamMemberIds)
+            ? teamMembers.filter((teamMember: TeamMemberModel) => {
+                return task.teamMemberIds.includes(teamMember.id);
+              })
+            : [];
+
+          return { ...task, teamMembers: teamMembersAssignedToTask };
+        })
     ),
     tap(() => {
       this._loadingTasksSubject.next(false);
@@ -58,6 +84,7 @@ export class CategoryDetailComponent implements OnDestroy {
     private _activatedRoute: ActivatedRoute,
     private _categoryService: CategoryService,
     private _taskService: TaskService,
+    private _teamMemberService: TeamMemberService,
     private _router: Router
   ) {}
 
@@ -67,18 +94,18 @@ export class CategoryDetailComponent implements OnDestroy {
   }
 
   deleteTask(id: string): void {
-    this._loadingDeleteTask.next(true);
+    this._loadingDeleteTask.next(id);
     this._taskService
       .delete(id)
       .pipe(
         take(1),
         catchError((err) => {
-          this._loadingDeleteTask.next(true);
+          this._loadingDeleteTask.next(null);
           throw err;
         })
       )
       .subscribe(() => {
-        this._loadingDeleteTask.next(true);
+        this._loadingDeleteTask.next(null);
         this._refreshTaskSubject.next();
       });
   }
