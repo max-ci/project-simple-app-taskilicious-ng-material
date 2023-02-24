c153c19648fd7a3c5e212094a0ba63701e0cb5cf
Maksymilian Czarnecki <maksymilian@czarnecki.me>
2023-02-24 20:14:09 +0100

    feat: add file service, update task model

---
+++ src/app/services/file.service-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { FileService } from './file.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [FileService],
+  exports: [],
+})
+export class FileServiceModule {}
